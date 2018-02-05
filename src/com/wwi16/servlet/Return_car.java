15096e385ec3e5186d894d190a7fe7358906356e
Marcel Ament <marcel.ament@mercatis.com>
2018-02-05 14:25:07 +0100

    renamed return to return_car

--- src/com/wwi16/servlet/Return_car.java
+++ src/com/wwi16/servlet/Return_car.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class Return_car extends HttpServlet {
 	
-    public Return() {
+    public Return_car() {
         super();
         // TODO Auto-generated constructor stub
     }
