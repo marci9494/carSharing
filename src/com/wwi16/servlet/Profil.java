a2b63675ba3c44060c31661dab4641d67f26028d
Marcel Ament <marcel.ament@mercatis.com>
2018-02-09 14:42:06 +0100

    improvements in profil

--- src/com/wwi16/servlet/Profil.java
+++ src/com/wwi16/servlet/Profil.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.wwi16.model.Nutzer;
 import com.wwi16.service.NutzerService;
@@ -34,9 +35,24 @@ public class Profil extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/profil.jsp");
 
-		NutzerService nutzerService = new NutzerService();
-		Nutzer nutzer = nutzerService.getNutzer("max.mustermann@muster.de");
-		request.setAttribute("nutzer",nutzer);
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			String userEmail = (String) session.getAttribute("userEmail");
+
+			if(userEmail != null){
+				
+				NutzerService nutzerService = new NutzerService();
+				Nutzer nutzer = nutzerService.getNutzer(userEmail);
+				request.setAttribute("nutzer",nutzer);
+				
+			}else{
+				//User nicht angemeldet was machen!?
+				
+			}
+		}
+		
+		
+
 		
 		dispatcher.forward(request, response);
 
