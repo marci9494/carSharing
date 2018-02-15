18c7e7b574e61ac37ac4d2716c70f53f78aa62c6
Marcel Ament <marcel.ament@mercatis.com>
2018-02-15 13:52:09 +0100

    created buchungen

---
+++ WebContent/WEB-INF/views/buchungen.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<link rel="stylesheet" type="text/css"
+	href="/carSharing/html/css/login.css" media="screen" />
+<jsp:include page="/theme/html/header.html" />
+
+<body>
+	<div>test123</div>
+
+</body>
+<jsp:include page="/theme/html/footer.html" />
+
+</html>
---
+++ src/com/wwi16/servlet/Buchungen.java
@@ -0,0 +1,18 @@
+package com.wwi16.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Buchungen extends HttpServlet{
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/buchungen.jsp");
+		dispatcher.forward(request, response);
+	} 
+
+}
