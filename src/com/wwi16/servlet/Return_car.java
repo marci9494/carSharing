e024e5d06cc6606f976fbf9e9d8095853c53fea8
hausercor <corinnahauser@t-online.de>
2018-03-05 13:03:43 +0100

    Kommentieren

--- src/com/wwi16/servlet/Return_car.java
+++ src/com/wwi16/servlet/Return_car.java
@@ -23,11 +23,13 @@ public class Return_car extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/return_car.jsp");
 		HttpSession session = request.getSession(false);
+		//Buchungsservice wird angelegt, damit die Funktion später verwendet werden kann
 		if (session != null) {
 			BuchungService buchungservice = new BuchungService();
-		
+			//Für den Willkommenstext wird die UserEmail geholt
 			String userEmail = (String) session.getAttribute("userEmail");
 			request.setAttribute("userEmail", userEmail);
+			//Für die Buchung wird die Buchungsid geholt
 			String buchungid = request.getParameter("id");
 			Buchung buchung = buchungservice.getBuchungById(buchungid);
 			
@@ -39,17 +41,23 @@ public class Return_car extends HttpServlet {
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		//Parameter werden auf die Werte aus der jsp gesetzt
 		String maengel = request.getParameter("maengel");
 		String nachricht = request.getParameter("nachricht");
 		String buchungid = request.getParameter("buchungid");
+		//Zur Überprüfung werden die Daten in der Konsole ausgegeben
 		System.out.println(maengel);
 		System.out.println(nachricht);
 		System.out.println(buchungid);
+		//Buchungsservice wird angelegt
 		BuchungService buchungservice = new BuchungService();
+		//Es wird nach entsprechender Buchung gesucht
 		Buchung buchung = buchungservice.getBuchungById(buchungid); 
+		//Daten aus Eingabe werden in die Buchung geschrieben
 		buchung.setNachricht(nachricht);
 		buchung.setMaengel(maengel);
+		//Buchung wird gespeichert
 		buchungservice.saveBuchung(buchung);
-		//Status auf abgeschlossen noch setzen
+		//Status auf abgeschlossen noch setzen!!!
 	}
 }
