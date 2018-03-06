31e7459967f4095689c0e34abb83b7bf38c2d36e
Jonas Ellinger <jonasellinger@users.noreply.github.com>
2018-03-07 00:04:54 +0100

    Update19: EIntragung von Kraftstoffen in DB

---
+++ WebContent/WEB-INF/sql/update19.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `kraftstoff` CHANGE `NAME` `NAME` VARCHAR(11) NOT NULL;
+INSERT INTO `kraftstoff` (`ID`, `NAME`) VALUES ('1', 'Benzin');
+INSERT INTO `kraftstoff` (`ID`, `NAME`) VALUES ('2', 'Diesel');
+INSERT INTO `kraftstoff` (`ID`, `NAME`) VALUES ('3', 'Elektro');
+INSERT INTO `kraftstoff` (`ID`, `NAME`) VALUES ('4', 'Hybrid');
+INSERT INTO `kraftstoff` (`ID`, `NAME`) VALUES ('5', 'Erdgas');
+INSERT INTO `kraftstoff` (`ID`, `NAME`) VALUES ('6', 'sonstige');
