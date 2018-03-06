ca8c4ca06eb679dc677f0aaf691f45a40a44d3d3
Jonas Ellinger <jonasellinger@users.noreply.github.com>
2018-03-07 00:26:08 +0100

    Update19: EIntragung von Kraftstoffen in DB II

--- WebContent/WEB-INF/sql/update19.sql
+++ WebContent/WEB-INF/sql/update19.sql
@@ -5,3 +5,8 @@ INSERT INTO `kraftstoff` (`ID`, `NAME`) VALUES ('3', 'Elektro');
 INSERT INTO `kraftstoff` (`ID`, `NAME`) VALUES ('4', 'Hybrid');
 INSERT INTO `kraftstoff` (`ID`, `NAME`) VALUES ('5', 'Erdgas');
 INSERT INTO `kraftstoff` (`ID`, `NAME`) VALUES ('6', 'sonstige');
+ALTER TABLE `kraftstoff` CHANGE `ID` `ID` BIGINT(11) NOT NULL;
+ALTER TABLE `fahrzeug` DROP `kraftstoff`;
+ALTER TABLE `fahrzeug` ADD `KRAFTSTOFF` BIGINT(11) NOT NULL AFTER `MODELL`;
+ALTER TABLE `fahrzeug` ADD CONSTRAINT `fahrzeug_ibfk_5` FOREIGN KEY (`KRAFTSTOFF`) 
+REFERENCES `kraftstoff`(`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
