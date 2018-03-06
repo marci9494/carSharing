fa334593763ac4f07568ffac3b9e5ee643e54e7a
Jonas Ellinger <jonasellinger@users.noreply.github.com>
2018-03-06 19:22:08 +0100

    Update18: Eintragung von FahrzeugFarben

---
+++ WebContent/WEB-INF/sql/update18.sql
@@ -0,0 +1,6 @@
+DELETE FROM `fahrzeug_farbe` WHERE `fahrzeug_farbe`.`ID` = 1;
+INSERT INTO `fahrzeug_farbe` (`ID`, `NAME`) VALUES ('1', 'Grau');
+INSERT INTO `fahrzeug_farbe` (`ID`, `NAME`) VALUES ('2', 'Rot');
+INSERT INTO `fahrzeug_farbe` (`ID`, `NAME`) VALUES ('3', 'Schwarz');
+INSERT INTO `fahrzeug_farbe` (`ID`, `NAME`) VALUES ('4', 'Weiﬂ');
+INSERT INTO `fahrzeug_farbe` (`ID`, `NAME`) VALUES ('5', 'sonstige');
